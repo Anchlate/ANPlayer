312b498b57d708fcaba92122275a042d199a7b6b
Qianrun <Qianrun@qianrundemini.lan>
2017-03-07 09:57:29 +0800

    Initial Commit

---
+++ ANPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ANPlayer
+//
+//  Created by Qianrun on 17/3/7.
+//  Copyright © 2017年 qianrun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
